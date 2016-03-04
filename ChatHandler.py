b5d3a9307ba8d3e5c779ac3398179ebc91c1afb3
Tone Winsnes <tonekathrinew@gmail.com>
2016-03-04 12:03:29 +0100

    la til add

--- ChatHandler.py
+++ ChatHandler.py
@@ -14,7 +14,7 @@ class ChatHandler:
         self.active_users.remove(user)
 
     def addHistory(self, chat_line):
-        self.chat_history.remove(chat_line)
+        self.chat_history.add(chat_line)
 
     def deleteHistory(self):
         self.chat_history = []
