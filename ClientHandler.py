c884bb4d9d55526da244a47804b94c16ee26c10b
mytterland <maritytterland@gmail.com>
2016-03-04 12:20:25 +0100

    La ClientHandler i egen fil

---
+++ ClientHandler.py
@@ -0,0 +1,26 @@
+__author__ = 'mytterland'
+
+import socketserver
+
+class ClientHandler(socketserver.BaseRequestHandler):
+    """
+    This is the ClientHandler class. Everytime a new client connects to the
+    server, a new ClientHandler object will be created. This class represents
+    only connected clients, and not the server itself. If you want to write
+    logic for the server, you must write it outside this class
+    """
+
+    def handle(self):
+        """
+        This method handles the connection between a client and the server.
+        """
+        self.ip = self.client_address[0]
+        self.port = self.client_address[1]
+        self.connection = self.request
+
+        # Loop that listens for messages from the client
+        while True:
+            received_string = self.connection.recv(4096)
+
+
+            # TODO: Add handling of received payload from client
--- Server.py
+++ Server.py
@@ -7,30 +7,6 @@ Variables and functions that must be used by all the ClientHandler objects
 must be written here (e.g. a dictionary for connected clients)
 """
 
-class ClientHandler(socketserver.BaseRequestHandler):
-    """
-    This is the ClientHandler class. Everytime a new client connects to the
-    server, a new ClientHandler object will be created. This class represents
-    only connected clients, and not the server itself. If you want to write
-    logic for the server, you must write it outside this class
-    """
-
-    def handle(self):
-        """
-        This method handles the connection between a client and the server.
-        """
-        self.ip = self.client_address[0]
-        self.port = self.client_address[1]
-        self.connection = self.request
-
-        # Loop that listens for messages from the client
-        while True:
-            received_string = self.connection.recv(4096)
-
-
-            # TODO: Add handling of received payload from client
-
-
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     """
     This class is present so that each client connected will be ran as a own
