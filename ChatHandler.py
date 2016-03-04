78b271c12748e671cc901f5b3c5892f666fe3990
mytterland <maritytterland@gmail.com>
2016-03-04 13:49:41 +0100

    Fikset Chathandler (add/remove connection), har lagt til request- og content-sjekker i ClientHandler. Ikke komplett.

--- ChatHandler.py
+++ ChatHandler.py
@@ -2,6 +2,9 @@ __author__ = 'macsita'
 
 class ChatHandler:
 
+    this_user = None
+    connections = []
+
     def __init__(self, active_users, chat_history):
         self.active_users = active_users
         self.chat_history = chat_history
@@ -9,9 +12,11 @@ class ChatHandler:
 
     def addUser(self, user):
         self.active_users.add(user)
+        self.this_user = user
 
-    def removeUser(self, user):
-        self.active_users.remove(user)
+    # def removeUser(self):
+    #     self.active_users.remove(self.this_user)
+    #     self.this_user = None
 
     def addHistory(self, chat_line):
         self.chat_history.add(chat_line)
@@ -25,4 +30,9 @@ class ChatHandler:
     def getHistory(self):
         return self.chat_history
 
+    def addConnection(self, chatHandler):
+        self.connections.append(chatHandler)
+
+    def removeConnection(self, chatHandler):
+        self.connections.remove(chatHandler)
 
--- ClientHandler.py
+++ ClientHandler.py
@@ -1,6 +1,8 @@
 __author__ = 'mytterland'
 
 import socketserver
+import ChatHandler
+import json
 
 class ClientHandler(socketserver.BaseRequestHandler):
     """
@@ -9,6 +11,7 @@ class ClientHandler(socketserver.BaseRequestHandler):
     only connected clients, and not the server itself. If you want to write
     logic for the server, you must write it outside this class
     """
+    chatHandler = ChatHandler()
 
     def handle(self):
         """
@@ -20,7 +23,61 @@ class ClientHandler(socketserver.BaseRequestHandler):
 
         # Loop that listens for messages from the client
         while True:
-            received_string = self.connection.recv(4096)
-
+            self.received_string = self.connection.recv(4096)
+            self.checkPayload()
 
             # TODO: Add handling of received payload from client
+
+    def checkPayload(self):
+    #     self.recieved_string = self.received_string.split(',')
+    #     if self.recieved_string[0].matches("'request': <"+"login"+">") and self.received_string[1].matches("'content': <"+"[a-zA-z0-9_]+"+">"):
+    #         username = self.received_string[1][11:self.received_string[1].length - 1]
+    #         if username not in self.chatHandler.getActive_users():
+    #             self.chatHandler.addUser(username)
+    #             print(self.chatHandler.getHistory())
+    #         else:
+    #             print("brukernavnet er tatt, velg et annet")
+    #
+    #
+    #     if self.recieved_string[0].matches("'request': <"+"logout"+">") and not self.received_string[1][11:self.received_string[1].length - 1]:
+    #         self.chatHandler.removeUser()
+        req = None
+        cont = None
+
+        if type(self.received_string) != str:
+            try:
+                jrec = json.loads(self.received_string)
+                req = jrec["request"].encode()
+                cont = jrec["content"].encode()
+            except ValueError:
+                print("Dette er ikke et JSON-objekt")
+
+        else:
+            jrec = json.loads(self.received_string)
+            req = jrec["request"]
+            cont = jrec["content"]
+
+        if req == "login" and cont:
+            user = cont
+            if user not in self.chatHandler.getUsers():
+                self.chatHandler.addUser(user)
+                self.chatHandler.addConnection(self.chatHandler)
+                print(self.chatHandler.getHistory())
+            else:
+                print("Brukernavnet er opptatt, velg et annet.")
+
+        if req == "logout" and not cont:
+            self.chatHandler.removeUser()
+            self.chatHandler.removeConnection(self.chatHandler)
+
+        if req == "msg" and cont:
+            self.chatHandler.addHistory()
+            self.connection.send(cont)
+
+        if req == "names" and not cont:
+            print(self.chatHandler.getUsers())
+
+        if req == "help" and not cont:
+            print("hjelpetext")
+
+
