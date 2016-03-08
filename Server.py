753ff636a79929575827ac19e3406a8fa6aec6c0
Sandra <sandraskarshaug@gmail.com>
2016-03-08 13:03:58 +0100

    Ednring på ip-adresse

--- Server.py
+++ Server.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
 
     No alterations are necessary
     """
-    HOST, PORT = 'localhost', 9998
+    HOST, PORT = '', 9998
     print('Server running...')
 
     # Set up and initiate the TCP server
